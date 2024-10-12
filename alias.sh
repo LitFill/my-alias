1fe8ace4f7bf22e62c0bd0b73f41f20beb3e7d0e
LitFill <marrazzy54@gmail.com>
2024-10-12 15:32:24 +0700

    feat(alias+keybind): add some

--- alias.sh
+++ alias.sh
@@ -1,1 +1,6 @@
-alias echoed='echo hello LitFill'
+alias tses='sesh connect "$(sesh list | fzf)"'
+alias ls='ls --color'
+alias lll='ls -alrt'
+
+bindkey -s '^h' 'tses\n'
+bindkey -s '^n' 'nvim .\n'
