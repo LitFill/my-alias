0ca234274e1a7188c7ec52b5c51fdaaa39ac2916
LitFill <marrazzy54@gmail.com>
2024-11-09 19:52:18 +0700

    feat(aliases): add tree(erd), so, :q, n., algrep

--- alias.sh
+++ alias.sh
@@ -4,3 +4,24 @@ alias lll='ls -alrt'
 
 bindkey -s '^h' 'tses\n'
 bindkey -s '^n' 'nvim .\n'
+
+# tree
+alias tree='erd --disk-usage line --icons --layout inverted'
+alias treee='erd --long --disk-usage line --icons --time mod --layout inverted'
+alias tree1='tree -L 1'
+alias tree2='tree -L 2'
+
+alias sysup='sudo -p "Key for system-upgrade: *****" \
+  zypper dup \
+  && echo \
+  "\n\t┌─────────────────────────┐\
+   \n\t│ System-upgrade success! │\
+   \n\t└─────────────────────────┘\n"'
+
+alias :q='exit'
+
+alias n.='nvim .'
+
+alias so='source ~/.zshrc'
+
+alias algrep='alias | rg'
