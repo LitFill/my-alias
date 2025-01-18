ac8b3b24da7b90b53d7f751df5665e790fba0a51
LitFill <marrazzy54@gmail.com>
2025-01-18 15:40:56 +0700

    feat(alias): add newdir and co.

--- alias.sh
+++ alias.sh
@@ -11,7 +11,7 @@ alias treee='erd --long --disk-usage line --icons --time mod --layout inverted'
 alias tree1='tree -L 1'
 alias tree2='tree -L 2'
 
-alias sysup='sudo -p "Key for system-upgrade: *****" \
+alias sysup='sudo -p "Key for system-upgrade: ***** " \
   zypper dup \
   && echo \
   "\n\t┌─────────────────────────┐\
@@ -26,5 +26,48 @@ alias so='source ~/.zshrc'
 
 alias algrep='alias | rg'
 
+# alias fmths='fourmolu -i --column-limit=80 --function-arrows=leading-args'
+
+alias untar='tar -zxvf'
+
+## create a new directory and move into it immediately
+newdir() {
+	mkdir -p "$1" && cd "$1"
+}
+
+## Log the installed packages via zypper
+export LOG_ZYPPER=${LOG_ZYPPER:-./zypper-install-all.sh}
+
+# Ensure log file exists and is initialized
+initialize_log_file() {
+	if [ ! -f "$LOG_ZYPPER" -o ! -s "$LOG_ZYPPER" ]; then
+		echo "Creating or initializing log file: $LOG_ZYPPER"
+		echo "#!/usr/bin/env sh" >"$LOG_ZYPPER"
+		echo "" >>"$LOG_ZYPPER"
+		echo "set -euxo pipefail" >>"$LOG_ZYPPER"
+		echo "" >>"$LOG_ZYPPER"
+		echo "sudo zypper install \\" >>"$LOG_ZYPPER"
+	fi
+}
+
+log-zypper() {
+	initialize_log_file
+
+	append_to_log() {
+		# Append each argument to the log file
+		for arg in "$@"; do
+			echo -e "\t$arg \\" >>"$LOG_ZYPPER"
+		done
+	}
+
+	# Run zypper and log installed packages if successful
+	if sudo zypper install "$@"; then
+		append_to_log "$@"
+	else
+		echo "Error: Failed to install packages: $@" >&2
+	fi
+}
+
 alias zy='zypper'
 alias _zy='sudo zypper'
+alias zyin='log-zypper'
